f41fc2034ce728cac20cf8d0c2bcd505a649db85
DESKTOP-ATB3NLE\Kirill <kiryakonev01@mail.ru>
2021-03-25 22:27:29 +0500

    Add 1-6 lab to my repository. Create ue4 blank project. Project's name MiddleAgesGame

---
+++ Project/ue4/MiddleAgesGame/Source/MiddleAgesGame/MiddleAgesGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiddleAgesGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiddleAgesGame, "MiddleAgesGame" );
